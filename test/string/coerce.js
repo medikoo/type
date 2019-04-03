48a9a859fe5b82cdbecca1a14fc7f1cfcc20f510
Mariusz Nowak <medyk@medikoo.com>
2019-04-03 18:57:47 +0200

    test: improve var name

--- test/string/coerce.js
+++ test/string/coerce.js
@@ -1,34 +1,36 @@
 "use strict";
 
-var assert       = require("chai").assert
-  , stringCoerce = require("../../string/coerce");
+var assert         = require("chai").assert
+  , coerceToString = require("../../string/coerce");
 
 describe("string/coerce", function () {
-	it("Should return input string", function () { assert.equal(stringCoerce("foo"), "foo"); });
-	it("Should coerce numbers", function () { assert.equal(stringCoerce(12), "12"); });
-	it("Should coerce booleans", function () { assert.equal(stringCoerce(true), "true"); });
+	it("Should return input string", function () { assert.equal(coerceToString("foo"), "foo"); });
+	it("Should coerce numbers", function () { assert.equal(coerceToString(12), "12"); });
+	it("Should coerce booleans", function () { assert.equal(coerceToString(true), "true"); });
 	it("Should coerce string objects", function () {
-		assert.equal(stringCoerce(new String("bar")), "bar");
+		assert.equal(coerceToString(new String("bar")), "bar");
 	});
 	it("Should coerce objects", function () {
 		assert.equal(
-			stringCoerce({ toString: function () { return "Some object"; } }), "Some object"
+			coerceToString({ toString: function () { return "Some object"; } }), "Some object"
 		);
 	});
 	if (typeof Object.create === "function") {
 		it("Should not coerce objects with no toString", function () {
-			assert.equal(stringCoerce(Object.create(null)), null);
+			assert.equal(coerceToString(Object.create(null)), null);
 		});
 	}
 	it("Should not coerce objects with no custom toString", function () {
-		assert.equal(stringCoerce({}), null);
+		assert.equal(coerceToString({}), null);
 	});
-	it("Should not coerce null", function () { assert.equal(stringCoerce(null), null); });
-	it("Should not coerce undefined", function () { assert.equal(stringCoerce(undefined), null); });
+	it("Should not coerce null", function () { assert.equal(coerceToString(null), null); });
+	it("Should not coerce undefined", function () {
+		assert.equal(coerceToString(undefined), null);
+	});
 
 	if (typeof Symbol === "function") {
 		it("Should not coerce symbols", function () {
-			assert.equal(stringCoerce(Symbol("foo")), null);
+			assert.equal(coerceToString(Symbol("foo")), null);
 		});
 	}
 });
