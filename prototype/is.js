3998348d3da16845cddc7858c11aade71ac1e6f5
Mariusz Nowak <medyk@medikoo.com>
2019-04-04 17:18:52 +0200

    feat: ensure to not crash

--- prototype/is.js
+++ prototype/is.js
@@ -4,6 +4,10 @@ var isObject = require("../object/is");
 
 module.exports = function (value) {
 	if (!isObject(value)) return false;
-	if (!value.constructor) return false;
-	return value.constructor.prototype === value;
+	try {
+		if (!value.constructor) return false;
+		return value.constructor.prototype === value;
+	} catch (error) {
+		return false;
+	}
 };
