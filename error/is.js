96ef399fb776c5105b0a3d750d97af95cdad4b06
Mariusz Nowak <mariusz@medikoo.com>
2019-08-06 12:49:21 +0200

    fix: Recognize host errors (e.g. DOMException)

--- error/is.js
+++ error/is.js
@@ -2,8 +2,10 @@
 
 var isPrototype = require("../prototype/is");
 
-var objectToString = Object.prototype.toString
-  , objectTaggedString = objectToString.call(new Error());
+var objectToString = Object.prototype.toString;
+
+// Recognized host specific errors (e.g. DOMException)
+var errorTaggedStringRe = /^\[object .*(?:Error|Exception)\]$/;
 
 module.exports = function (value) {
 	if (!value) return false;
@@ -16,9 +18,11 @@ module.exports = function (value) {
 	}
 
 	// Ensure its native Error object (has [[ErrorData]] slot)
-	// Note: it's not 100% precise as string tag may be overriden
-	// and other objects can be hacked to expose it
-	if (objectToString.call(value) !== objectTaggedString) return false;
+	// Note: There seems no 100% bulletproof way of doing that as:
+	// - In ES2015+ string tag can be overriden via Symbol.toStringTag
+	// - Host errors do not share native error tag. Still we rely on assumption that
+	//   tag for each error will end either with `Error` or `Exception` string
+	if (!errorTaggedStringRe.test(objectToString.call(value))) return false;
 
 	return !isPrototype(value);
 };
