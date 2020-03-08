17ae4cfa3c30ff1ae83d33c6de58de77a04d5f72
Mariusz Nowak <mariusz@medikoo.com>
2020-03-09 12:41:41 +1300

    test: Fix typo

--- test/lib/resolve-error-message.js
+++ test/lib/resolve-error-message.js
@@ -7,7 +7,7 @@ describe("lib/resolve-error-message", function () {
 	it("Should insert value", function () {
 		assert.equal(resolveErrorMessage("%v is invalid", 12), "12 is invalid");
 	});
-	it("Should support custome error message via inputOptions.errorMessage", function () {
+	it("Should support custom error message via inputOptions.errorMessage", function () {
 		assert.equal(
 			resolveErrorMessage("%v is invalid", null, { errorMessage: "%v is not supported age" }),
 			"null is not supported age"
